54c0dd1f2fc27324d74143678336c37a50d87af0
Rodrigo Martinez <rodrigotmarz@gmail.com>
2024-07-22 19:39:43 -0600

    Agregar archivos de proyecto.

---
+++ Fig/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Ingrese un numero");
+int a = int.Parse(Console.ReadLine());
+string s = "0";
+if (a < 1) s = "01";
+for (int i = 1; i < a; i++)
+{
+    int serie;
+    serie = 0;
+    for (int j = 0; j < i; j++)
+    {
+        serie = serie * 10 + i;
+    }
+
+    s += serie.ToString();
+}
+if (a == 0) Console.WriteLine("0");
+if (a == 1) a += 1;
+for (int i = 0; i < a; i++)
+{
+    Console.Write(s[i]);
+}
